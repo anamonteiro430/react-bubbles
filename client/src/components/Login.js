c261a4814a6d3247678f0e4aed7ea24944d01760
kika <anamonteiro430@gmail.com>
2020-01-31 16:17:17 +0000

    setup login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+	const { credentials, setCredentials } = useState({
+		username: '',
+		password: ''
+	});
+
+	// make a post request to retrieve a token from the api
+	// when you have handled the token, navigate to the BubblePage route
+	const handleSubmit = e => {
+		e.preventDefault();
+	};
+
+	return (
+		<div>
+			<h1>Welcome to the Bubble App!</h1>
+			<p>Build a login page here</p>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					name='username'
+					value={credentials.username}
+					onChange={handleChanges}
+				/>
+				<input
+					type='password'
+					name='password'
+					value={credentials.password}
+					onChange={handleChanges}
+				/>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
